46e8149dd82876a645f99bfbb13e2762fafa1388
Romanschek, Enrico <enrico.romanschek@htw-dresden.de>
2021-08-06 16:18:14 +0200

    New

--- code/topology-relations/DE9IM_Test/Program.cs
+++ code/topology-relations/DE9IM_Test/Program.cs
@@ -15,10 +15,9 @@ namespace DE9IM_Test
         {
             string[] args = new[]
             {
-                @"D:\WSR\Testdaten\Geometries.txt",
-                @"D:\WSR\Testdaten\Scales.txt",
-                @"D:\WSR\Testdaten\Translation.txt",
-                @"results.csv"
+                @"Geometries.txt",
+                @"Transformations.txt",
+                "results.csv"
             };
             //Einlesen der Ausgangswerte, die vergleichenden Objekte und Muster-Matrix
             FileProcess.ReadData(args[0], out var geometryComparisons);
@@ -30,7 +29,7 @@ namespace DE9IM_Test
             //Rotation der Daten
             //TransformationCalculation.Rotation(args[2], geometryComparisons, countInitialComparisons);
             //Translation der Daten
-            TransformationCalculation.Translation(args[2], geometryComparisons, countInitialComparisons);
+            TransformationCalculation.Translation(args[1], geometryComparisons, countInitialComparisons);
 
             //Einzelne Bibliotheken aufrufen und DE9IM Test durchführen
             //PostGIS postgis = new PostGIS("Host=localhost;Port=5432;User Id=postgres;Password=toor;");
@@ -48,7 +47,7 @@ namespace DE9IM_Test
             SpatiaLite spatialite = new SpatiaLite("Data Source=database.sqlite;Version=3;New=True;Compress=True;");
             spatialite.CheckDE9IM(geometryComparisons);
 
-            FileProcess.WriteComparisonFile(geometryComparisons, args[3]);
+            FileProcess.WriteComparisonFile(geometryComparisons, args[2]);
             FileProcess.Analyse(geometryComparisons);
             //Console.WriteLine("Press key to exit");
             //Console.ReadKey(true); //So the user close the console
